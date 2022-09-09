70e5fa39673f766fa2f1773a9ff7421de96e481f
Артем Аверьянов <artemaveranov@MacBook-Pro.local>
2022-09-09 14:57:44 +0300

    add simple notification for different android API

---
+++ app/src/main/java/com/example/servicestest/MyForegroundService.kt
@@ -0,0 +1,56 @@
+package com.example.servicestest
+
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+import kotlinx.coroutines.*
+
+class MyForegroundService: Service() {
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
+
+    override fun onBind(p0: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        log("OnCreate")
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        log("onStartCommand")
+        val start = intent?.getIntExtra(EXTRA_START, 0) ?: 0
+        coroutineScope.launch {
+            for (i in start until start + 100) {
+                delay(1000)
+                log("Timer $i")
+            }
+        }
+        // START_STICKY - пересоздаст сервис с intent равным 0
+        // START_NOT_STICKY - не пересоздаст сервис
+        // START_REDELIVER_INTENT - пересоздаст сервис с нашим intent
+        return START_REDELIVER_INTENT
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        log("onDestroy")
+        coroutineScope.cancel()
+    }
+
+    private fun log(message: String) {
+        Log.d("SERVICE_TAG", "MyService: $message")
+    }
+
+    companion object {
+        private const val EXTRA_START = "start"
+        fun newIntent(context: Context, startNumber: Int): Intent {
+            return Intent(context, MyForegroundService::class.java).apply {
+                putExtra(EXTRA_START, startNumber)
+            }
+        }
+    }
+
+}
